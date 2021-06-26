15a3a6abfaf735f1f2d4325aec70bbb793750df9
Henio <henioajunior@gmail.com>
2021-06-26 09:10:40 -0300

    Inserindo valores

--- src/main/java/br/com/alura/springdata/SpringDataApplication.java
+++ src/main/java/br/com/alura/springdata/SpringDataApplication.java
@@ -1,13 +1,31 @@
 package br.com.alura.springdata;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.alura.springdata.orm.Cargo;
+import br.com.alura.springdata.repository.CargoRepository;
+
 @SpringBootApplication
-public class SpringDataApplication {
+public class SpringDataApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataApplication.class, args);
 	}
-
+	
+	private final CargoRepository repository;
+	
+	public SpringDataApplication(CargoRepository repository) {
+		this.repository = repository;
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		Cargo cargo = new Cargo();
+		cargo.setDescricao("DESENVOLVEDOR DE SOFTWARE");
+		
+		repository.save(cargo);
+		
+	}
 }
